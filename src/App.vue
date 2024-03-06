163071fa6f250854d042cdcaeb5e341069bfb976
lamp-la <13657274737@163.com>
2024-03-06 18:21:51 +0800

    login

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-   <!-- 路由的出口，不能删 -->
-    <router-view/>
-    <h1>黑码 头条</h1>
+    <!-- 路由的出口，不能删 -->
+    <router-view />
+   <login></login>
   </div>
 </template>
 <script>
-
-
+import login from './views/login/index.vue'
 export default {
- name:'App'
-}
+  name: "App",
+  components:{
+    login,
+  }
+};
 </script>
 
 
 <style lang="less">
-
 </style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="login-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="page-nav-bar" title="登陆" />
+    <!-- 登陆表单 -->
+    <van-form @submit="onSubmit">
+      <van-field
+        v-model="username"
+        name="用户名"
+        placeholder="请输入手机号"
+        left-icon="smile-o"
+        :rules="[{ required: true, message: '请填写用户名' }]"
+      >
+        <i slot="left-icon" class="toutiao toutiao-shouji"></i>
+      </van-field>
+      <van-field
+        v-model="password"
+        type="password"
+        name="密码"
+        placeholder="请输入验证码"
+        :rules="[{ required: true, message: '请填写验证码' }]"
+      >
+        <i slot="left-icon" class="toutiao toutiao-yanzhengma"></i>
+        <template #button>
+          <van-button class="send-sms-btn" round size="small" type="default"
+            >发送验证码</van-button
+          >
+        </template>
+      </van-field>
+      <div class="login-btn-wrap">
+        <van-button class="login-btn" block type="info" native-type="submit"> 
+            登录
+         </van-button>
+      </div>
+    </van-form>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginPage",
+  components: {},
+  props: {},
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {
+    onSubmit(values) {
+      console.log("submit", values);
+    },
+  },
+};
+</script>
+
+<style scoped lang="less">
+.login-container {
+  .toutiao {
+    font-size: 37px;
+  }
+
+  .send-sms-btn {
+    width: 152px;
+    height: 46px;
+    line-height: 46px;
+    background-color: #ededed;
+    font-size: 22px;
+    color: #666;
+  }
+  .login-btn-wrap {
+    padding: 53px 33px;
+    .login-btn {
+        background-color: #6db4fb;
+        border: none
+    }
+  }
+}
+</style>
